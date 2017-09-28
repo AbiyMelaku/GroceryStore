e3fa16b5fae99cb2ad7bea30e6a718c5cf2026a2
Abiy Melaku <agirma08@gmail.com>
2017-09-27 20:01:29 -0700

    Grocery List component

---
+++ client/src/components/GroceryList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GroceryItem from './GroceryItem.jsx';
+
+const GroceryList = (props) => (
+  <div className="groceries">
+    { props.list.map(item => <GroceryItem item={item} key={item.id}/>)}
+  </div>
+)
+
+export default GroceryList;
