b3ec7e20c86dc731c02fb0c7cffba57139768ad4
Abiy Melaku <agirma08@gmail.com>
2017-09-27 20:00:48 -0700

    Grocery Item component

---
+++ client/src/components/GroceryItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GroceryItem = (props) => (
+  <div>
+    <span> { props.item.description } </span>
+    <span> { props.item.quantity } </span>
+  </div>
+)
+
+export default GroceryItem;
